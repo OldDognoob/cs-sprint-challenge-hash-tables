e50a88d32a8cdf8066e0b06b6eab79e5a9bb9094
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-07-10 13:17:27 +0300

    Merging two Packages

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,24 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    # The Strategy:
+    # How to approach the problem:
+    # create a weight_dict = {}
+    weight_dict = {}
+    # Check if already exists in weight_dict
+    for i in range(length): 
+        # create a variable value were limited_weight is equal with the limit weight - weight indexes
+        limited_weight = (limit-weights[i])
+        value = weight_dict.get(limited_weight)
+        #if we find the value in weight_dict list:
+        if value is not None:
+            #can return a tuple with the index
+            return (i, value)
+         #If there is not a value in weight_dict list           
+        else:
+        #Set key to current value, value at index
+            weight_dict[weights[i]] = i
+    print(weight_dict)
+   
 
     return None
